83cc668678a108b2e0148f9a1ace1c6102bf3db1
myaryhin0201 <yarygin45@gmail.com>
2020-12-27 15:34:15 +0100

    add main tasks

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 import images from "./gallery-items.js";
 const gallery = document.querySelector('ul.js-gallery');
-const lightbox = document.querySelector('lightbox');
+const lightbox = document.querySelector('div.lightbox');
+const lightImage = document.querySelector('.lightbox__image');
+const btnClose = document.querySelector('button[data-action="close-lightbox"]');
+const lightOverlay = document.querySelector('.lightbox__overlay');
 
+let i = 0;
+let activeIndex = 0;
 const createImage = (image) => {
     const listRef = document.createElement('li');
     const linkRef = document.createElement('a');
@@ -12,15 +17,68 @@ const createImage = (image) => {
     picture.setAttribute('src', image.preview);
     picture.setAttribute('alt', image.description);
     picture.setAttribute('data-source', image.original);
+    picture.setAttribute('data-index', i);
     linkRef.appendChild(picture);
     listRef.appendChild(linkRef);
+    i += 1;
     return listRef;
 }
 const items = images.map(image => createImage(image));
 gallery.append(...items);
-console.log(gallery);
+
+const onArrowRight = (event) => {
+    if (event.key === 'ArrowRight') {
+        activeIndex += 1;
+        console.log(activeIndex);
+    }
+}
+const onArrowLeft = (event) => {
+    if (event.key === 'ArrowLeft') {
+        activeIndex -= 1;
+        console.log(activeIndex);
+    }
+}
+const onEscape = (event) => {
+    if (event.key === 'Escape') {
+        closeModal();
+    }
+}
+const openModal = (event) => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    } 
+    lightbox.classList.add('is-open');
+    const selectTarget = event.target;
+    activeIndex = Number(selectTarget.dataset.index);
+    setLargeImgAttr(selectTarget);
+    gallery.removeEventListener('click', openModal);
+    window.addEventListener("keydown", onEscape);
+    window.addEventListener("keydown", onArrowLeft);
+    window.addEventListener("keydown", onArrowRight);
+    btnClose.addEventListener('click', closeModal);
+    lightOverlay.addEventListener('click', closeModal);
+}
+const setLargeImgAttr = (selectTarget) => {
+    lightImage.setAttribute('src', selectTarget.dataset.source);
+    lightImage.setAttribute('alt', selectTarget.alt);
+}
+const closeModal = () => {
+    lightbox.classList.remove('is-open');
+    lightImage.setAttribute('src', '');
+    lightImage.setAttribute('alt', '');
+    gallery.addEventListener('click', openModal);
+    window.removeEventListener("keydown", onEscape);
+    window.removeEventListener("keydown", onArrowLeft);
+    window.removeEventListener("keydown", onArrowRight);
+    btnClose.removeEventListener('click', closeModal);
+    lightOverlay.removeEventListener('click', closeModal);
+}
+gallery.addEventListener('click', openModal);
 
 
-// gallery.addEventListener('click', openModal());
+
+
+
 
 
